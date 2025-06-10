49708bf313587270659688757b79dee87d2869ae
VickoT <vicko.tornblom@gmail.com>
2025-06-10 13:24:53 +0200

    clean Mantel R script

--- Mantel_test/01_do_all.R
+++ Mantel_test/01_do_all.R
@@ -11,15 +11,20 @@ library(vegan)
 # Load input files
 #------------------------------------------------------------------------------
 
-#Copy Number Variation (CNV)–normalized abundance counts, Karla's Qiime2 pipeline
+# Copy Number Variation (CNV)–normalized abundance counts (from Qiime2 pipeline)
 df <- read_csv("_raw/all_taxonomy_abundance_CNV.csv")
 
-# Tree file by Courtney, the 5 Briviate strains (unrooted)
+# Tree file, the 5 Briviate strains (unrooted)
 tree <- read.tree("_raw/18S_align_mask.fasta.MFPuf.treefile")
 
 
+# Script seting
+#------------------------------------------------------------------------------
+
+set.seed(42)
+
 # Process data
-#------------------------------§------------------------------------------------
+#------------------------------------------------------------------------------
 
 # Rename tree tip labels to match strain names
 tree$tip.label <- c("PCE", "FB10N2", "LRM1b", "PCB", "LRM2N6")
@@ -31,32 +36,20 @@ phylo_dist <- cophenetic(tree)
 # Bray-Curtis (Arcobacter-only) + PCoA Visualization
 #------------------------------------------------------------------------------
 
-"d__Bacteria;p__Proteobacteria;c__Gammaproteobacteria;o__Vibrionales;f__Vibrionaceae;g__Vibrio"
-"d__Bacteria;p__Campilobacterota;c__Campylobacteria;o__Campylobacterales;f__Arcobacteraceae;"
-"d__Bacteria;p__Campilobacterota;c__Campylobacteria;o__Campylobacterales;f__Arcobacteraceae;g__Malaciobacter"
-"d__Bacteria;p__Firmicutes;c__Clostridia;o__Peptostreptococcales-Tissierellales;f__Fusibacteraceae;g__Fusibacter"
-"d__Bacteria;p__Desulfobacterota;c__Desulfovibrionia;o__Desulfovibrionales;f__Desulfovibrionaceae;g__Desulfovibrio"
-
+# Filter the abundance data to only inlcude the arcobacter species
 taxa_cols <- grep("f__Arcobacteraceae", colnames(df), value = TRUE)
 print(taxa_cols)
 taxa_table <- df %>%
   select(index, strain, all_of(taxa_cols))
 
-taxa_table %>%
-  rowwise() %>%
-  mutate(total_abundance = sum(c_across(where(is.numeric)), na.rm = TRUE)) %>%
-  ungroup() %>%
-  select(index, strain, total_abundance) %>%
-  filter(total_abundance > 0) %>% 
-  count(strain)
 
-# 2. Calculate Bray-Curtis distances between samples
+# Calculate Bray-Curtis distances between samples
 taxa_dist <- taxa_table %>%
   column_to_rownames("index") %>%
   select(-strain) %>%
   vegdist(method = "bray")
 
-# 3. PCoA
+# PCoA
 pcoa_result <- cmdscale(taxa_dist, k = 2, eig = TRUE)
 eig <- pcoa_result$eig
 var_explained <- eig / sum(eig) * 100
@@ -65,7 +58,7 @@ pcoa_df <- as.data.frame(pcoa_result$points) %>%
   rownames_to_column("index") %>%
   left_join(select(taxa_table, index, strain), by = "index")
 
-# 4. Plot
+# Plot
 ggplot(pcoa_df, aes(x = V1, y = V2, color = strain)) +
   geom_point(size = 3) +
   theme_minimal() +
@@ -75,9 +68,10 @@ ggplot(pcoa_df, aes(x = V1, y = V2, color = strain)) +
     y = paste0("PCoA 2 (", round(var_explained[2], 1), "%)"),
     color = "Strain"
   )
+ggsave("figures/pcoa_arcobacter.svg", width = 6, height = 5)
 
 #------------------------------------------------------------------------------
-# Summarize data per strain (mean) + Bray-Curtis between strains
+# Calculate mean Bray-Curtis / strain - to use in Mantel test
 #------------------------------------------------------------------------------
 
 df_strain <- taxa_table %>%
@@ -100,6 +94,15 @@ bray_strain <- vegdist(df_strain, method = "bray")
 mantel_result <- mantel(phylo_dist, bray_strain, method = "pearson", permutations = 999)
 mantel_result
 
-  
+#Mantel statistic based on Pearson's product-moment correlation 
+
+#Mantel statistic r: 0.2502 
+#      Significance: 0.3 
+#Upper quantiles of permutations (null model):
+#  90%   95% 97.5%   99% 
+#0.463 0.529 0.573 0.586 
+#Permutation: free
+#Number of permutations: 119
+
 
 
